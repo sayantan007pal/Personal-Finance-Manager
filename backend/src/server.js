1b686d4c8aa54824e9995c00d6abf37585298c3e
sayantanpal <sayantanpal100@gmail.com>
2026-02-12 15:20:29 +0530

    feat: set up backend server with Express and MongoDB integration
    - Added server.js to initialize Express application and connect to MongoDB
    - Configured CORS and body-parser middleware for handling requests
    - Updated package.json with new scripts for starting and developing the server
    - Included necessary dependencies for server functionality

---
+++ backend/src/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 3005;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}))
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, async() => {
+    console.log(`Server is running on port ${PORT}`);
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+    finally {
+        console.log(`Server is running on port ${PORT} along with MongoDB`);
+    }
+});
