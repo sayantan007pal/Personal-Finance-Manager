d1d2e86412d2b9add9d5078bef1126f03acd9adb
sayantanpal <sayantanpal100@gmail.com>
2026-02-13 03:37:44 +0530

    feat: add index file for model exports
    - Created index.js to centralize model exports, starting with User model for better organization and accessibility in the backend structure.

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.schema({
+    username:{
+        type:String,
+        required:[true, "Username is Required"],
+        unique:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:[true, "Email is Required"],
+        unique:true,
+        trim:true
+    },
+    fullNmae:{
+        type:String,
+        required:[true, "Full Name is Required" ],
+        trim:true
+    },
+    password:{
+        type:String,
+        required: [true, "Password is Required"]
+    },
+    isEmailVerified:{
+        type:Boolean,
+        default:false,
+    },
+    refreshToken:{
+        type:String,
+    },
+    forgotPasswordToken:{
+        type:String,
+    },
+    forgotPasswordExpiry:{
+        type:Date,
+    },
+    emailVerificationToken:{
+        type:String,
+    },
+    emailVerificationExpiry:{
+        type:Date,
+    }
+},
+{
+    timestamps:true
+})
+
+
+
+const User = mongoose.model('users', userSchema)
+export default User
