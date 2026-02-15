061063de4b614f26539db1a97fe47cfc48ba6ad1
sayantanpal <sayantanpal100@gmail.com>
2026-02-16 00:09:42 +0530

    feat: Implement account creation API and add a new dashboard page.

--- backend/src/services/accountService.js
+++ backend/src/services/accountService.js
@@ -1,1 +1,48 @@
 import { User, Account } from "../models/index.js";
+
+const AccountType = ["SAVINGS", "CURRENT", "CREDIT_CARD", "LOAN", "OTHER"]
+
+//account service
+async function createAccount(req, res){
+    try {
+        const user = await User.findById(req.user.id);
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        const {name, type, balance, currency, bankName, accountName, accountType, accountNumberLastFour, linkedAt} = req.body;
+        if(!AccountType.includes(type)){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid account type"
+            });
+        }
+        const newAccount = new Account({
+            name,
+            type,
+            balance,
+            currency,
+            bankName,
+            accountName,
+            accountType,
+            accountNumberLastFour,
+            linkedAt,
+            userId:req.user.id
+        });
+        const savedAccount = await newAccount.save();
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully",
+            data: savedAccount
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+}
+
+export { createAccount };
